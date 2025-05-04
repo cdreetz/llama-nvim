4e773551f4042532410dc867e01687ada8c0ca16
Christian Reetz <cdreetz@gmail.com>
2025-05-04 00:30:43 -0700

    ok fr i need to get back to work

--- lua/llama-nvim/init.lua
+++ lua/llama-nvim/init.lua
@@ -105,6 +105,7 @@ end
 
 -- Function to generate code
 function M.generate_code(opts)
+	local buffer = ""
 	local prompt = opts.args
 	log("LlamaGenerate prompt: " .. prompt)
 
@@ -124,22 +125,34 @@ function M.generate_code(opts)
 
 	call_llama_api_stream(messages, function(content)
 		if content then
+			buffer = buffer .. content
 			log("Received content chunk: " .. content)
-			local new_lines = vim.split(content, "\n", true)
-			for i, line in ipairs(new_lines) do
-				if i == 1 then
-					-- Append to the current line
-					local current_line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1]
-					vim.api.nvim_buf_set_lines(0, row - 1, row, false, { current_line .. line })
-				else
-					-- Insert new lines
-					vim.api.nvim_buf_set_lines(0, row, row, false, { line })
-					row = row + 1
+			local new_lines = vim.split(buffer, "\n", true)
+
+			-- Process complete lines, keep last line in buffer if incomplete
+			if #new_lines > 1 then
+				-- Process all lines except the last one (which might be incomplete)
+				for i = 1, #new_lines - 1 do
+					if i == 1 then
+						-- Append to the current line
+						local current_line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1]
+						vim.api.nvim_buf_set_lines(0, row - 1, row, false, { current_line .. new_lines[i] })
+					else
+						-- Insert new lines
+						vim.api.nvim_buf_set_lines(0, row, row, false, { new_lines[i] })
+						row = row + 1
+					end
 				end
+				-- Keep the last line in buffer
+				buffer = new_lines[#new_lines]
 			end
 			vim.api.nvim_win_set_cursor(0, { row, col })
 		else
-			-- End of stream
+			-- End of stream - write any remaining buffer content
+			if buffer ~= "" then
+				local current_line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1]
+				vim.api.nvim_buf_set_lines(0, row - 1, row, false, { current_line .. buffer })
+			end
 			vim.api.nvim_echo({ { "Code generation complete.", "Normal" } }, false, {})
 			log("Code generation complete")
 		end
